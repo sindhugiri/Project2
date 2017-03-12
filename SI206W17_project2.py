18e66eb9885c2bd67f5675f2ccbb1f991a3b6733
Sindhu Giri <sindhgir@umich.edu>
2017-03-12 19:10:25 -0400

    Final Commit

--- SI206W17_project2.py
+++ SI206W17_project2.py
@@ -2,7 +2,7 @@
 
 ## COMMENT HERE WITH:
 ## Your name: Sindhu Giri 
-## Anyone you worked with on this project:
+## Anyone you worked with on this project: Madi Atkins
 
 ## Below we have provided import statements, comments to separate out the parts of the project, instructions/hints/examples, and at the end, tests. See the PDF of instructions for more detail. 
 ## You can check out the SAMPLE206project2_caching.json for an example of what your cache file might look like.
